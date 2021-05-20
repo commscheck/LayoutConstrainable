eb157a9f309d12cdf9956a2e933ddb6560ce3247
Benjamin Lea <benjamin@canva.com>
2021-05-20 19:10:11 +1000

    Add edge enums

--- Sources/LayoutConstrainable/LayoutConstrainable.swift
+++ Sources/LayoutConstrainable/LayoutConstrainable.swift
@@ -1,5 +1,31 @@
 import UIKit
 
+public enum LayoutEdge {
+    case top
+    case bottom
+    case left
+    case right
+}
+
+extension Set where Element == LayoutEdge {
+    public static let all: Self = [.top, .bottom, .left, .right]
+    public static let horizontal: Self = [.left, .right]
+    public static let vertical: Self = [.top, .bottom]
+}
+
+public enum DirectionalLayoutEdge {
+    case top
+    case bottom
+    case leading
+    case trailing
+}
+
+extension Set where Element == DirectionalLayoutEdge {
+    public static let all: Self = [.top, .bottom, .leading, .trailing]
+    public static let horizontal: Self = [.leading, .trailing]
+    public static let vertical: Self = [.top, .bottom]
+}
+
 public protocol LayoutConstrainable {
     var bottomAnchor: NSLayoutYAxisAnchor { get }
     var centerXAnchor: NSLayoutXAxisAnchor { get }
@@ -24,34 +50,36 @@ extension UILayoutGuide: LayoutConstrainable {}
 
 extension LayoutConstrainable {
 
-    @discardableResult public func constrainEdges(to other: LayoutConstrainable, insetBy insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
+    @discardableResult public func constrainEdges(_ edges: Set<LayoutEdge> = .all, to other: LayoutConstrainable, insetBy insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
         (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
 
         let constraints = [
-            topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top),
-            bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom),
-            leftAnchor.constraint(equalTo: other.leftAnchor, constant: insets.left),
-            rightAnchor.constraint(equalTo: other.rightAnchor, constant: -insets.right),
-        ]
+            edges.contains(.top) ? topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top) : nil,
+            edges.contains(.bottom) ? bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom) : nil,
+            edges.contains(.left) ? leftAnchor.constraint(equalTo: other.leftAnchor, constant: insets.left) : nil,
+            edges.contains(.right) ? rightAnchor.constraint(equalTo: other.rightAnchor, constant: -insets.right) : nil,
+        ].compactMap { $0 }
+
         NSLayoutConstraint.activate(constraints)
         return constraints
     }
 
-    @discardableResult public func constrainEdges(to other: LayoutConstrainable, insetBy inset: CGFloat) -> [NSLayoutConstraint] {
+    @discardableResult public func constrainEdges(_ edges: Set<LayoutEdge> = .all, to other: LayoutConstrainable, insetBy inset: CGFloat) -> [NSLayoutConstraint] {
         let insets = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
-        return constrainEdges(to: other, insetBy: insets)
+        return constrainEdges(edges, to: other, insetBy: insets)
     }
 
     @available(iOS 11.0, tvOS 11.0, *)
-    @discardableResult public func constrainEdges(to other: LayoutConstrainable, insetBy insets: NSDirectionalEdgeInsets) -> [NSLayoutConstraint] {
+    @discardableResult public func constrainEdges(_ edges: Set<DirectionalLayoutEdge> = .all, to other: LayoutConstrainable, insetBy insets: NSDirectionalEdgeInsets) -> [NSLayoutConstraint] {
         (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
 
         let constraints = [
-            topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top),
-            bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom),
-            leadingAnchor.constraint(equalTo: other.leadingAnchor, constant: insets.leading),
-            trailingAnchor.constraint(equalTo: other.trailingAnchor, constant: -insets.trailing),
-        ]
+            edges.contains(.top) ? topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top) : nil,
+            edges.contains(.bottom) ? bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom) : nil,
+            edges.contains(.leading) ? leadingAnchor.constraint(equalTo: other.leadingAnchor, constant: insets.leading) : nil,
+            edges.contains(.trailing) ? trailingAnchor.constraint(equalTo: other.trailingAnchor, constant: -insets.trailing) : nil,
+        ].compactMap { $0 }
+
         NSLayoutConstraint.activate(constraints)
         return constraints
     }

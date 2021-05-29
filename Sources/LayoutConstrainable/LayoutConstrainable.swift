de35431c3db8709b120dbf303a9c854dc2f7d639
Benjamin Lea <benjamin@canva.com>
2021-05-29 15:55:39 +1000

    Add comments

--- Sources/LayoutConstrainable/LayoutConstrainable.swift
+++ Sources/LayoutConstrainable/LayoutConstrainable.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/// An edge of a rectangle.
 public enum LayoutEdge {
     case top
     case bottom
@@ -13,6 +14,11 @@ extension Set where Element == LayoutEdge {
     public static let vertical: Self = [.top, .bottom]
 }
 
+/// An edge of a rectangle, taking reading direction into account.
+///
+/// For LTR languages (e.g. English), `.leading` is equivalent to left, and `.trailing` is equivalent to right.
+/// For RTL languages (e.g. Arabic) these are reversed.
+///
 public enum DirectionalLayoutEdge {
     case top
     case bottom
@@ -26,6 +32,10 @@ extension Set where Element == DirectionalLayoutEdge {
     public static let vertical: Self = [.top, .bottom]
 }
 
+/// A type that participates in auto layout.
+///
+/// Contains the auto layout properties and methods common to UIView and UILayoutGuide.
+///
 public protocol LayoutConstrainable {
     var bottomAnchor: NSLayoutYAxisAnchor { get }
     var centerXAnchor: NSLayoutXAxisAnchor { get }
@@ -50,9 +60,21 @@ extension UILayoutGuide: LayoutConstrainable {}
 
 extension LayoutConstrainable {
 
+    /// Creates and activates constraints between the specified edges of the receiver and `other`.
+    ///
+    /// - Parameters:
+    ///   - edges: Which edges to add constraints for. Defaults to all edges.
+    ///   - other: The other view or layout guide to constrain the receiver's edges to.
+    ///   - insets: Offsets the receiver's edges towards the rectangle's center
+    ///             (or away from center if values are negative).
+    ///             Edges not in `edges` are ignored.
+    ///             Defaults to zero.
+    ///
+    /// - Returns: The added constraints.
+    ///            The constraints are already active,
+    ///            but you may wish to hold references to deactivate or modify them later.
+    ///
     @discardableResult public func constrainEdges(_ edges: Set<LayoutEdge> = .all, to other: LayoutConstrainable, insetBy insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
-        (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
-
         let constraints = [
             edges.contains(.top) ? topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top) : nil,
             edges.contains(.bottom) ? bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom) : nil,
@@ -64,15 +86,40 @@ extension LayoutConstrainable {
         return constraints
     }
 
+    /// Creates and activates constraints between the specified edges of the receiver and `other`.
+    ///
+    /// - Parameters:
+    ///   - edges: Which edges to add constraints for. Defaults to all edges.
+    ///   - other: The other view or layout guide to constrain the receiver's edges to.
+    ///   - inset: Offsets the receiver's edges towards the rectangle's center
+    ///            (or away from center if values are negative).
+    ///            Edges not in `edges` are ignored.
+    ///
+    /// - Returns: The added constraints.
+    ///            The constraints are already active,
+    ///            but you may wish to hold references to deactivate or modify them later.
+    ///
     @discardableResult public func constrainEdges(_ edges: Set<LayoutEdge> = .all, to other: LayoutConstrainable, insetBy inset: CGFloat) -> [NSLayoutConstraint] {
         let insets = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
         return constrainEdges(edges, to: other, insetBy: insets)
     }
 
+    /// Creates and activates constraints between the specified edges of the receiver and `other`,
+    /// taking reading direction into account.
+    ///
+    /// - Parameters:
+    ///   - edges: Which edges to add constraints for. Defaults to all edges.
+    ///   - other: The other view or layout guide to constrain the receiver's edges to.
+    ///   - insets: Offsets the receiver's edges towards the rectangle's center
+    ///             (or away from center if values are negative).
+    ///             Edges not in `edges` are ignored.
+    ///
+    /// - Returns: The added constraints.
+    ///            The constraints are already active,
+    ///            but you may wish to hold references to deactivate or modify them later.
+    ///
     @available(iOS 11.0, tvOS 11.0, *)
     @discardableResult public func constrainEdges(_ edges: Set<DirectionalLayoutEdge> = .all, to other: LayoutConstrainable, insetBy insets: NSDirectionalEdgeInsets) -> [NSLayoutConstraint] {
-        (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
-
         let constraints = [
             edges.contains(.top) ? topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top) : nil,
             edges.contains(.bottom) ? bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: -insets.bottom) : nil,
@@ -84,9 +131,19 @@ extension LayoutConstrainable {
         return constraints
     }
 
+    /// Creates and activates constraints between the centers of the receiver and `other`.
+    ///
+    /// - Parameters:
+    ///   - other: The other view or layout guide to constrain the receiver's center to.
+    ///   - offset: Offsets the receiver's center to the left for positive `x` values,
+    ///             and down for postive `y` values.
+    ///             Defaults to zero.
+    ///
+    /// - Returns: The added constraints.
+    ///            The constraints are already active,
+    ///            but you may wish to hold references to deactivate or modify them later.
+    ///
     @discardableResult public func constrainCenter(to other: LayoutConstrainable, offsetBy offset: CGPoint = .zero) -> [NSLayoutConstraint] {
-        (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
-
         let constraints = [
             centerXAnchor.constraint(equalTo: other.centerXAnchor, constant: offset.x),
             centerYAnchor.constraint(equalTo: other.centerYAnchor, constant: offset.y),
@@ -95,9 +152,19 @@ extension LayoutConstrainable {
         return constraints
     }
 
-    @discardableResult public func constrainSize(toEqual other: LayoutConstrainable, scale: CGFloat = .zero) -> [NSLayoutConstraint] {
-        (self as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
-
+    /// Creates and activates constraints between the size of the receiver and `other`.
+    ///
+    /// - Parameters:
+    ///   - other: The other view or layout guide to constrain the receiver's size to.
+    ///   - scale: Multiplier applied to the receiver's size.
+    ///            e.g. `2.0` will constrain the size of the receiver
+    ///            to twice the width and height of `other.`
+    ///
+    /// - Returns: The added constraints.
+    ///            The constraints are already active,
+    ///            but you may wish to hold references to deactivate or modify them later.
+    ///
+    @discardableResult public func constrainSize(toEqual other: LayoutConstrainable, scale: CGFloat = 1.0) -> [NSLayoutConstraint] {
         let constraints = [
             widthAnchor.constraint(equalTo: other.widthAnchor, multiplier: scale),
             heightAnchor.constraint(equalTo: other.heightAnchor, multiplier: scale),
